3f25e33db2abf66251d7046b5d0720e07d489bfd
Liechtathlet <daniel.brun@gmx.net>
2015-04-12 12:02:36 +0200

    Update setup.sh

--- Linux/Setup/setup.sh
+++ Linux/Setup/setup.sh
@@ -12,3 +12,12 @@ apt-get install shutter -y
 
 ##Office
 apt-get install okular -y
+
+#Links
+##SSD
+###https://sites.google.com/site/easylinuxtipsproject/ssd
+###http://www.pcwelt.de/ratgeber/Optimales_SSD-Timing_fuer_Linux-Mehr_Tempo-8674837.html
+###http://community.linuxmint.com/tutorial/view/293
+##Linux Mint
+###https://sites.google.com/site/easylinuxtipsproject/mint-cinnamon-first
+###http://www.binarytides.com/better-linux-mint-17-cinnamon/
